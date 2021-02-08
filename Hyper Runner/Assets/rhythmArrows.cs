389c78d985944e271a6cefde9b9b2b545ed3d03d
des1gnerw1tch <zachcanon@gmail.com>
2021-02-07 23:03:59 -0500

    Removed a stray tile

--- Hyper Runner/Assets/rhythmArrows.cs
+++ Hyper Runner/Assets/rhythmArrows.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class rhythmArrows : MonoBehaviour
 {
     [SerializeField] private sunsetManager levelManager;
+    [SerializeField] private float playerFloatingSpeed;
 
     void OnTriggerEnter2D(Collider2D other) {
       if (other.CompareTag("Player")) {
         levelManager.SetPlayerMode("Rhythm");
+        levelManager.playerCamMoveSpeed = playerFloatingSpeed;
+        levelManager.UpdateSpeeds();
       }
     }
 }
