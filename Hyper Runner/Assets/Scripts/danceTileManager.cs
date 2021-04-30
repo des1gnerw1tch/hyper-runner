626e4df4aed2a1eb2458f4547514402d8b926b30
des1gnerw1tch <zachcanon@gmail.com>
2021-04-29 21:28:05 -0400

    Moved danceTileUpdater to its own script

---
+++ Hyper Runner/Assets/Scripts/danceTileManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// handles activating dance tiles, the once closest to the player
+public class danceTileManager : MonoBehaviour
+{
+  void Update() {
+    // Will update active dance key, on KeyUp because want to make sure
+    // first object when clicked is deleted first (which is on KeyDown)
+    if (Input.GetKeyUp("up") || Input.GetKey("down")) {
+      UpdateValidDanceKeys();
+    }
+  }
+
+  //Makes sure that only 1 dance key is okay to press at one time
+  void UpdateValidDanceKeys() {
+    danceObject[] objs = FindObjectsOfType<danceObject>();
+    if (objs.Length != 0) {
+      danceObject closest = objs[0];
+      foreach (danceObject obj in objs) {
+        if (obj.gameObject.GetComponent<Transform>().position.x < closest.GetComponent<Transform>().position.x) {
+          closest = obj;
+        }
+      }
+      closest.active = true;
+    }
+  }
+}
--- Hyper Runner/Assets/Scripts/rhythmArrows.cs
+++ Hyper Runner/Assets/Scripts/rhythmArrows.cs
@@ -9,7 +9,6 @@ public class rhythmArrows : MonoBehaviour
     [SerializeField] private float surroundingSpeedMultiplier;
 
     [SerializeField] private Transform player;
-    int i = 0;
 
     void OnTriggerEnter2D(Collider2D other) {
       if (other.CompareTag("Player")) {
@@ -17,30 +16,8 @@ public class rhythmArrows : MonoBehaviour
         levelManager.playerCamMoveSpeed = playerFloatingSpeed;
         Parallax.multiplier = surroundingSpeedMultiplier;
         levelManager.UpdateSpeeds();
-        UpdateValidDanceKeys();
+        //UpdateValidDanceKeys();
         //FindObjectOfType<CameraShake>().Begin(.5f, 15f, 1f);
-      }
-    }
-
-    void Update() {
-      // Will update active dance key, on KeyUp because want to make sure
-      // first object when clicked is deleted first (which is on KeyDown)
-      if (Input.GetKeyUp("up") || Input.GetKey("down")) {
-        UpdateValidDanceKeys();
-      }
-    }
-
-    //Makes sure that only 1 dance key is okay to press at one time
-    void UpdateValidDanceKeys() {
-      danceObject[] objs = FindObjectsOfType<danceObject>();
-      if (objs.Length != 0) {
-        danceObject closest = objs[0];
-        foreach (danceObject obj in objs) {
-          if (obj.gameObject.GetComponent<Transform>().position.x < closest.GetComponent<Transform>().position.x) {
-            closest = obj;
-          }
-        }
-        closest.active = true;
       }
     }
 }
