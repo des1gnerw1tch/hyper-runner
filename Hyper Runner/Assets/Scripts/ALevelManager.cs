96f436b73a687fabeabfd4e3595e650b717846b5
des1gnerw1tch <zachcanon@gmail.com>
2021-06-22 23:21:11 -0400

    small tweaks for results feature

--- Hyper Runner/Assets/Scripts/ALevelManager.cs
+++ Hyper Runner/Assets/Scripts/ALevelManager.cs
@@ -3,54 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public abstract class ALevelManager : MonoBehaviour, ILevelManager
-{
-  public float playerCamMoveSpeed = 5f;
-  public Move cameraMovement;
-  public PlayerMovement playerMovement;
-  public RhythmMovement rhythmMovement;
-  public PlayerInput input;
-  public Rigidbody2D player_rb;
-  public GameObject flyingParticles;
-  public float launchToRhythmSpeed = 5f;
+public abstract class ALevelManager : MonoBehaviour, ILevelManager {
+    public float playerCamMoveSpeed = 5f;
+    public Move cameraMovement;
+    public PlayerMovement playerMovement;
+    public RhythmMovement rhythmMovement;
+    public PlayerInput input;
+    public Rigidbody2D player_rb;
+    public GameObject flyingParticles;
+    public float launchToRhythmSpeed = 5f;
 
     // Start is called before the first frame update
-    public virtual void Start()
-    {
-      UpdateSpeeds();
+    public virtual void Start() {
+        UpdateSpeeds();
+        ResultsManager.init(); // initializes results of current game, which has just started
     }
 
     // UpdateSpeeds : Updates the values of speed for both the camera and the player
-    public virtual void UpdateSpeeds()  {
-      playerMovement.speed = playerCamMoveSpeed;
-      rhythmMovement.speed = playerCamMoveSpeed;
-      cameraMovement.speed = playerCamMoveSpeed;
+    public virtual void UpdateSpeeds() {
+        playerMovement.speed = playerCamMoveSpeed;
+        rhythmMovement.speed = playerCamMoveSpeed;
+        cameraMovement.speed = playerCamMoveSpeed;
     }
 
     // using either "Rhythm" or "Platformer" as mode, performs operations
     // to switch mode of game
-    public virtual void SetPlayerMode(string mode)  {
-      switch (mode) {
-        case "Platformer":
-          playerMovement.enabled = true;
-          playerMovement.SetAnimatorControllerAsPlatformer();
-          rhythmMovement.enabled = false;
-          input.SwitchCurrentActionMap("Player"); // switches action map to rhythm
-          player_rb.gravityScale = 5f;
-          flyingParticles.SetActive(false);
-          break;
-        case "Rhythm":
-          playerMovement.enabled = false;
-          rhythmMovement.enabled = true;
-          input.SwitchCurrentActionMap("Dance Keys"); // switches action map to rhythm
+    public virtual void SetPlayerMode(string mode) {
+        switch (mode) {
+            case "Platformer":
+                playerMovement.enabled = true;
+                playerMovement.SetAnimatorControllerAsPlatformer();
+                rhythmMovement.enabled = false;
+                input.SwitchCurrentActionMap("Player"); // switches action map to rhythm
+                player_rb.gravityScale = 5f;
+                flyingParticles.SetActive(false);
+                break;
+            case "Rhythm":
+                playerMovement.enabled = false;
+                rhythmMovement.enabled = true;
+                input.SwitchCurrentActionMap("Dance Keys"); // switches action map to rhythm
 
-          player_rb.gravityScale = 0;
-          flyingParticles.SetActive(true);
+                player_rb.gravityScale = 0;
+                flyingParticles.SetActive(true);
 
-          // this is how fast the player will jump into rhythym mode, will
-          // fix this bad system later
-          rhythmMovement.startRhythm(this.launchToRhythmSpeed);
-          break;
-      }
+                // this is how fast the player will jump into rhythym mode, will
+                // fix this bad system later
+                rhythmMovement.startRhythm(this.launchToRhythmSpeed);
+                break;
+        }
     }
 }
--- Hyper Runner/Assets/Scripts/danceObject.cs
+++ Hyper Runner/Assets/Scripts/danceObject.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-    // Normal Dance Tile, press a key to interact
-public class danceObject : ADanceObject
-{
+// Normal Dance Tile, press a key to interact
+public class danceObject : ADanceObject {
     private Transform player;
     private CharacterHealth characterHealth;
     [SerializeField]
@@ -23,91 +22,91 @@ public class danceObject : ADanceObject
     [SerializeField] private float camRumbleSpeed;
     [SerializeField] private float camRumbleDuration;
 
-    void Start()  {
-      player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-      characterHealth = FindObjectOfType<CharacterHealth>();
+    void Start() {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        characterHealth = FindObjectOfType<CharacterHealth>();
     }
 
-    void Update()
-    {
-      // TODO: get input for keyboard, will change to new input system later
-      if (Input.GetKeyDown(keyToPress) && active) {
-        Pressed();
-      }
-
-      // despawn object if missed
-      if ((player.position.x - transform.position.x) >= distanceUntilDestroy)  {
-        characterHealth.AddCharisma(-10f);
-        try {
-          FindObjectOfType<danceTileManager>().ActivateNextFowardKey();
-        } catch (Exception e) {
-          Debug.Log("tried to access dance tile manager when it was deactivated");
+    void Update() {
+        // TODO: get input for keyboard, will change to new input system later
+        if (Input.GetKeyDown(keyToPress) && active) {
+            Pressed();
         }
-        FindObjectOfType<AudioManager>().Play("negative");
-        SpawnScoreText(missedText); // spawn missed text pop up
-        Destroy(this.gameObject);
-      }
+
+        // despawn object if missed
+        if ((player.position.x - transform.position.x) >= distanceUntilDestroy) {
+            characterHealth.AddCharisma(-10f);
+            try {
+                FindObjectOfType<danceTileManager>().ActivateNextFowardKey();
+            }
+            catch (Exception e) {
+                Debug.Log("tried to access dance tile manager when it was deactivated");
+            }
+            FindObjectOfType<AudioManager>().Play("negative");
+            SpawnScoreText(missedText); // spawn missed text pop up
+            Destroy(this.gameObject);
+        }
     }
 
-    void Pressed()  {
+    void Pressed() {
 
-      float difference = Mathf.Abs(player.position.x - transform.position.x);
-      score = 10 - difference;
-      if (score < 0)
-        score = 0;
-      //Debug.Log("Score: " + score);
+        float difference = Mathf.Abs(player.position.x - transform.position.x);
+        score = 10 - difference;
+        if (score < 0)
+            score = 0;
+        //Debug.Log("Score: " + score);
 
-      if (score > 9.7)  {
-        SpawnScoreText(perfectText); // spawn perfect text
-        characterHealth.AddCharisma(10f);
-        FindObjectOfType<AudioManager>().Play("metronome");
-      } else if (score > 9.5) {
-        characterHealth.AddCharisma(3f);
-        SpawnScoreText(goodText); // spawn good text
-        FindObjectOfType<AudioManager>().Play("metronome");
-      } else if (score > 9) {
-        if (characterHealth.charisma > 50f) {
-          characterHealth.AddCharisma(-5f); // "okay" rating will only penalize if at high-charisma
-          FindObjectOfType<AudioManager>().Play("metronome");
+        if (score > 9.7) {
+            SpawnScoreText(perfectText); // spawn perfect text
+            characterHealth.AddCharisma(10f);
+            FindObjectOfType<AudioManager>().Play("metronome");
+        } else if (score > 9.5) {
+            characterHealth.AddCharisma(3f);
+            SpawnScoreText(goodText); // spawn good text
+            FindObjectOfType<AudioManager>().Play("metronome");
+        } else if (score > 9) {
+            if (characterHealth.charisma > 50f) {
+                characterHealth.AddCharisma(-5f); // "okay" rating will only penalize if at high-charisma
+                FindObjectOfType<AudioManager>().Play("metronome");
+            } else {
+                characterHealth.AddCharisma(3f);
+                FindObjectOfType<AudioManager>().Play("metronome");
+            }
+            FindObjectOfType<AudioManager>().Play("metronome");
+            SpawnScoreText(okText); // spawn ok text
         } else {
-          characterHealth.AddCharisma(3f);
-          FindObjectOfType<AudioManager>().Play("metronome");
+            characterHealth.AddCharisma(-10f);
+            SpawnScoreText(missedText); // spawn missed text pop up
+            FindObjectOfType<AudioManager>().Play("negative");
         }
-        FindObjectOfType<AudioManager>().Play("metronome");
-        SpawnScoreText(okText); // spawn ok text
-      } else  {
-        characterHealth.AddCharisma(-10f);
-        SpawnScoreText(missedText); // spawn missed text pop up
-        FindObjectOfType<AudioManager>().Play("negative");
-      }
 
-      Instantiate(destroyEffect, transform.position, Quaternion.identity);
-      FindObjectOfType<CameraShake>().Begin(camRumbleIntensity, camRumbleSpeed, camRumbleDuration);
-      Destroy(gameObject);
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        FindObjectOfType<CameraShake>().Begin(camRumbleIntensity, camRumbleSpeed, camRumbleDuration);
+        Destroy(gameObject);
     }
 
     void SpawnScoreText(GameObject text) {
-      var image = Instantiate(text) as GameObject;
-      image.transform.SetParent(canvas.transform, false);
+        var image = Instantiate(text) as GameObject;
+        image.transform.SetParent(canvas.transform, false);
 
-      // set the placement of these pop ups a little random
-      float wobble = 15;
-      float difX = UnityEngine.Random.Range(-wobble, wobble);
-      float difY = UnityEngine.Random.Range(-wobble, wobble);
-      image.GetComponent<RectTransform>().Translate(new Vector3(difX, difY, 0), Space.World);
+        // set the placement of these pop ups a little random
+        float wobble = 15;
+        float difX = UnityEngine.Random.Range(-wobble, wobble);
+        float difY = UnityEngine.Random.Range(-wobble, wobble);
+        image.GetComponent<RectTransform>().Translate(new Vector3(difX, difY, 0), Space.World);
     }
 
     // INPUT: Will be called from player object, as player is the only one with input connected
-    public override void OnUpDanceKeyPress()  {
-      if (keyToPress == "up" && active)  {
-        Pressed();
-      }
+    public override void OnUpDanceKeyPress() {
+        if (keyToPress == "up" && active) {
+            Pressed();
+        }
     }
 
-    public override void OnDownDanceKeyPress()  {
-      if (keyToPress == "down" && active) {
-        Pressed();
-      }
+    public override void OnDownDanceKeyPress() {
+        if (keyToPress == "down" && active) {
+            Pressed();
+        }
     }
 
 }
