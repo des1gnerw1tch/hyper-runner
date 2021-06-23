2b6b2d0c7fd7f4185da064756c2487c260abfc59
des1gnerw1tch <zachcanon@gmail.com>
2021-06-22 20:48:09 -0400

    Beginning of results screen, import TMP

--- Hyper Runner/Assets/ResultsManager.cs
+++ Hyper Runner/Assets/ResultsManager.cs
@@ -2,39 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResultsManager : MonoBehaviour {
+// this class keeps track of the current result of a level. Is used for "ResultsScreen" scene
+public static class ResultsManager {
 
-    private int countPlayerCrash; // how many times the player has run into an obstacle
-    private int countMissedDanceTiles; // how many times the player has missed a dance tile
-    private int countNonPerfectTiles; // how many times the player has not gotten a "perfect" tile
+    private static int countPlayerCrash; // how many times the player has run into an obstacle
+    private static int countMissedDanceTiles; // how many times the player has missed a dance tile
+    private static int countNonPerfectTiles; // how many times the player has not gotten a "perfect" tile
 
     // Start is called before the first frame update
-    void Start() {
-        this.countPlayerCrash = 0;
-        this.countMissedDanceTiles = 0;
-        this.countNonPerfectTiles = 0;
+    public static void init() {
+        countPlayerCrash = 0;
+        countMissedDanceTiles = 0;
+        countNonPerfectTiles = 0;
     }
 
     // When a player has run into an obstacle
     // EFFECT: increments playre crash by 1
-    public void IncPlayerCrash() {
-        this.countPlayerCrash++;
+    public static void IncPlayerCrash() {
+        countPlayerCrash++;
     }
 
     // When a player misses a dance tile
     // EFFECT: increments player missed dance tile, also incremements non perfect tiles
-    public void IncMissedDanceTiles() {
-        this.countMissedDanceTiles++;
-        this.countNonPerfectTiles++;
+    public static void IncMissedDanceTiles() {
+        countMissedDanceTiles++;
+        countNonPerfectTiles++;
     }
 
     // When a player does not score "Perfect" on a 
     // EFFECT: increments player non perfect tiles
-    public void IncNonPerfectTiles() {
-        this.countNonPerfectTiles++;
+    public static void IncNonPerfectTiles() {
+        countNonPerfectTiles++;
     }
-    /*
-    public string GenerateScore() {
-        
-    }*/
 }
