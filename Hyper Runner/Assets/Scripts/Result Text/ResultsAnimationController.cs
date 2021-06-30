db860955778f7df47e2902fa0fac9c3ab80c5f98
des1gnerw1tch <zachcanon@gmail.com>
2021-06-30 00:12:55 -0400

    Started score of results screen

--- Hyper Runner/Assets/Scripts/Result Text/ResultsAnimationController.cs
+++ Hyper Runner/Assets/Scripts/Result Text/ResultsAnimationController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ResultsAnimationController : MonoBehaviour {
     [SerializeField] private AResultText first;
+    [SerializeField] private ScoreText mobilityScore;
+    [SerializeField] private ScoreText rhythmScore;
     [SerializeField] private GameObject highScoreText;
 
     private void Start() {
+        this.SetScores();
         this.first.Activate();
         this.highScoreText.SetActive(false);
         ResultsManager.PrintResults();
@@ -17,5 +20,35 @@ public class ResultsAnimationController : MonoBehaviour {
         this.highScoreText.SetActive(true);
     }
 
+    // sets the scores for mobility and rhythm 
+    void SetScores() {
+        // TODO: Also calculate grade image here, would be cleaner (right now calculates in own script
+        float totalTiles = (float)ResultsManager.GetTotalTiles();
+        float nonPerfectTiles = (float)ResultsManager.GetNonPerfectTiles();
+        float missedTiles = (float)ResultsManager.GetMissedDanceTiles();
+
+        float perfectRatio = (totalTiles - nonPerfectTiles)
+            / totalTiles;
+        float hitRatio = (totalTiles - missedTiles)
+            / totalTiles;
+
+        // this crazy math makes a score, somewhere in the 1000 range
+        // the longer the level (more tiles) and the least crashes, gives the most points. 
+        this.mobilityScore.score =
+            2500 - ResultsManager.GetPlayerCrashes() * 500;
+
+        // score for rhythm
+        int rhythmScoreTemp =
+            (int)Mathf.Ceil(2500 - ((totalTiles - nonPerfectTiles) * 2) - (missedTiles * 3));
+        if (rhythmScoreTemp < 0) {
+            this.rhythmScore.score = 0;
+        } else {
+            this.rhythmScore.score = rhythmScoreTemp;
+
+        }
+
+
+
+    }
 
 }
