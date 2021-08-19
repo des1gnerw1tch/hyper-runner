0584fb971c44c7736ca97bb3eb53c4d63da2c5c7
des1gnerw1tch <zachcanon@gmail.com>
2021-08-19 16:21:33 -0400

    added replace game objects wizard

---
+++ Hyper Runner/Assets/Scripts/ReplaceGameObjects.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+// CopyComponents - by Michael L. Croswell for Colorado Game Coders, LLC
+// March 2010
+
+//Modified by Kristian Helle Jespersen
+//June 2011
+
+// Modified by Zachary Walker-Liang
+// August 2021
+
+public class ReplaceGameObjects : ScriptableWizard {
+    public bool copyValues = true;
+    public GameObject NewType;
+    public GameObject[] OldObjects;
+
+    [MenuItem("Custom/Replace GameObjects")]
+
+
+    static void CreateWizard() {
+        ScriptableWizard.DisplayWizard("Replace GameObjects", typeof(ReplaceGameObjects), "Replace");
+    }
+
+    void OnWizardCreate() {
+        //Transform[] Replaces;
+        //Replaces = Replace.GetComponentsInChildren<Transform>();
+
+        foreach (GameObject go in OldObjects) {
+            GameObject newObject;
+            newObject = (GameObject)PrefabUtility.InstantiatePrefab(NewType);
+            newObject.SetActive(go.activeSelf); // will disable new prefab object if needed
+            newObject.transform.position = go.transform.position;
+            newObject.transform.rotation = go.transform.rotation;
+            newObject.transform.parent = go.transform.parent;
+
+            DestroyImmediate(go);
+
+        }
+
+    }
+}
