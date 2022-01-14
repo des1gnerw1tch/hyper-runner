9a00c2aab9fa7a36c92b8482e7cd5fb3475134ef
des1gnerw1tch <zachcanon@gmail.com>
2022-01-13 23:22:08 -0500

    Indicators move smoothly between pause menu options

--- Hyper Runner/Assets/Scripts/PauseMenu/IndicatorGroup.cs
+++ Hyper Runner/Assets/Scripts/PauseMenu/IndicatorGroup.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEditor;
+using UnityEditor.Experimental;
 using UnityEngine;
 
 public class IndicatorGroup : MonoBehaviour
@@ -7,11 +9,14 @@ public class IndicatorGroup : MonoBehaviour
     [SerializeField] private RectTransform[] indicators;
     [SerializeField] private RectTransform[] buttons; // this should be the locations of the buttons
     [SerializeField] private AInGamePauseButton[] buttonsActions; // related to the action this button does
+    [SerializeField] private float indicatorMoveSpeed;
     
     [Header("Auto Completed Serialization")]
     [SerializeField] private UIInputHandler uiInputHandler;
     
-    private int currentSelection;
+    private int currentSelection; // the current button selection 
+    private float indicatorYPos;
+    private Coroutine lastRoutine;
 
     private void Start()
     {
@@ -28,11 +33,64 @@ public class IndicatorGroup : MonoBehaviour
 
     void UpdateIndicatorPosition()
     {
-        foreach (RectTransform indicator in indicators)
+        if (!Mathf.Approximately(indicators[0].position.y, buttons[currentSelection].position.y))
         {
-            Vector3 pos = new Vector3(indicator.position.x, buttons[currentSelection].position.y, indicator.position.z);
-            indicator.position = pos;
+            indicatorYPos = buttons[currentSelection].position.y;
+            if (lastRoutine != null)
+            {
+                StopCoroutine(lastRoutine);
+            }
+            lastRoutine = StartCoroutine("MoveIndicatorsToNewPosition");
         }
+    }
+    
+    /// <summary>
+    /// Moves Indicators to new position once dictated by an input. Was very tedious to make, took me around 1 hour!
+    /// </summary>
+    IEnumerator MoveIndicatorsToNewPosition()
+    {
+        bool processDone = false;
+        while (!processDone)
+        {
+            if (!Mathf.Approximately(indicators[0].transform.position.y, indicatorYPos))
+            {
+                float increment;
+                float step = indicatorMoveSpeed * Time.unscaledDeltaTime;
+                if (indicatorYPos > indicators[0].transform.position.y)
+                {
+                    if (indicators[0].transform.position.y + step > indicatorYPos)
+                    {
+                        increment = indicatorYPos - indicators[0].transform.position.y;
+                        processDone = true;
+                    }
+                    else
+                    {
+                        increment =  step;
+                    }
+                }
+                else
+                {
+                    if (indicators[0].transform.position.y -  step < indicatorYPos)
+                    {
+                        increment = -(indicators[0].transform.position.y - indicatorYPos);
+                        processDone = true;
+                    }
+                    else
+                    {
+                        increment = -step;
+                    }
+                }
+            
+                foreach (RectTransform indicator in indicators)
+                {
+                    indicator.transform.position = new Vector3(indicator.transform.position.x, 
+                        indicator.transform.position.y + increment, indicator.transform.position.z);
+                }
+            }
+            
+            yield return new WaitForEndOfFrame();
+        } 
+        
     }
     void OnUp()
     {
