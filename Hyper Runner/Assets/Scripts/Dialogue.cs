1f657c86d5ec5b822826fb3a391baf76d2da74dd
des1gnerw1tch <zachcanon@gmail.com>
2022-08-24 04:37:03 -0400

    Two dialogue possibilities for robot

--- Hyper Runner/Assets/Scripts/Dialogue.cs
+++ Hyper Runner/Assets/Scripts/Dialogue.cs
@@ -7,11 +7,13 @@ using Random = UnityEngine.Random;
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tmp;
-    [SerializeField] private string textToDisplay;
+    [SerializeField] private string[] textToDisplay;
 
     [SerializeField] private float delayBetweenEachCharacter;
 
     private AudioManager audioManager;
+
+    private int interactCount = 0;
     
     private void Start()
     {
@@ -21,25 +23,30 @@ public class Dialogue : MonoBehaviour
     public void StartDialogue()
     {
         tmp.text = "";
-        StartCoroutine(DisplayNextCharacter(0));
+        StartCoroutine(DisplayNextCharacter(0, textToDisplay[interactCount]));
+        interactCount++;
+        if (interactCount == textToDisplay.Length)
+        {
+            interactCount = 0;
+        }
     }
 
-    private IEnumerator DisplayNextCharacter(int curChar)
+    private IEnumerator DisplayNextCharacter(int curChar, string _textToDisplay)
     {
-        if (curChar > textToDisplay.Length)
+        if (curChar > _textToDisplay.Length)
         {
             DialogueEnded();
             yield break;
         }
         
-        if (curChar != 0 && !Char.IsWhiteSpace(textToDisplay, curChar - 1))
+        if (curChar != 0 && !Char.IsWhiteSpace(_textToDisplay, curChar - 1))
         {
             yield return new WaitForSeconds(delayBetweenEachCharacter);
             PlayRandomRobotSound();
         }
 
-        tmp.text = textToDisplay.Substring(0, curChar);
-        StartCoroutine(DisplayNextCharacter(curChar + 1));
+        tmp.text = _textToDisplay.Substring(0, curChar);
+        StartCoroutine(DisplayNextCharacter(curChar + 1, _textToDisplay));
     }
 
     private void DialogueEnded()
