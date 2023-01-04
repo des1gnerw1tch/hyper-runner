456b56e46f2590bddc7fbfba507d790ccaafe1d4
des1gnerw1tch <zachcanon@gmail.com>
2023-01-04 18:35:58 -0500

    Characters show locked or unlocked sprites in change character UI

--- Hyper Runner/Assets/Scripts/SelectableUIElements/CharacterSelectableElement.cs
+++ Hyper Runner/Assets/Scripts/SelectableUIElements/CharacterSelectableElement.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using Characters;
 using SaveFileSystem;
-using UnityEngine.UI;
 
 namespace SelectableUIElements
 {
@@ -11,16 +10,32 @@ namespace SelectableUIElements
     public class CharacterSelectableElement : ASelectableElement
     {
         [SerializeField] private PlayableCharacterEnum character;
+        [SerializeField] private GameObject lockedCharacterDisplay;
+        [SerializeField] private GameObject unlockedCharacterDisplay;
+        
         [SerializeField] private GameObject selectedBorder;
         private const string CHARACTER_SELECTED_BUT_NOT_UNLOCKED_SOUND = "characterLocked";
         
         // To change current character sprite and animator.
         [SerializeField] private CurrentCharacterPanel currentCharacterPanel;
-        
 
+        private void OnEnable()
+        {
+            if (IsCharacterLocked())
+            {
+                lockedCharacterDisplay.SetActive(true);
+                unlockedCharacterDisplay.SetActive(false);
+            }
+            else
+            {
+                lockedCharacterDisplay.SetActive(false);
+                unlockedCharacterDisplay.SetActive(true);
+            }
+        }
+        
         public override void Selected()
         {
-            if (!GameDataManager.Instance.IsCharacterUnlocked(character))
+            if (IsCharacterLocked())
             {
                 FindObjectOfType<AudioManager>().Play(CHARACTER_SELECTED_BUT_NOT_UNLOCKED_SOUND);
                 return;
@@ -42,5 +57,7 @@ namespace SelectableUIElements
         {
             selectedBorder.SetActive(false);
         }
+
+        private bool IsCharacterLocked() => !GameDataManager.Instance.IsCharacterUnlocked(character);
     }
 }
