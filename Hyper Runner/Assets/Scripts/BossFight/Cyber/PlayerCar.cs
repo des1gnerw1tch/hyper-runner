630144d585ae7faf485235f0b8aa7477fc0f6dba
des1gnerw1tch <zachcanon@gmail.com>
2024-08-07 02:03:21 -0400

    Collision with NPC drivers

--- Hyper Runner/Assets/Scripts/BossFight/Cyber/PlayerCar.cs
+++ Hyper Runner/Assets/Scripts/BossFight/Cyber/PlayerCar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace BossFight.Cyber
@@ -11,29 +12,42 @@ namespace BossFight.Cyber
         [SerializeField] private Transform playerCam;
         [SerializeField] private float camTiltAngleWhileTurn;
         [SerializeField] private StreetBounds streetBounds;
+        [SerializeField] private float crashStunTime;
+        [SerializeField] private float velocityAfterCrash;
         
         [SerializeField]
         private float velocity = 0;
 
+        private bool playerStunned = false;
+        
         private void Update()
         {
-            if (Input.GetKey("w"))
+            if (Input.GetKey("w") && !playerStunned)
             {
                 velocity += acceleration;
             }
-            if (Input.GetKey("s"))
+            if (Input.GetKey("s") && !playerStunned)
             {
                 velocity -= brakeAcceleration;
             }
-
-            bool turningLeft = Input.GetKey("a");
-            bool turningRight = Input.GetKey("d");
             
             this.transform.Translate(Vector3.back * velocity *  Time.deltaTime);
+            if (playerStunned)
+            {
+                return;
+            }
+            HandlePlayerTurning();
+        }
+
+        private void HandlePlayerTurning()
+        {
             Vector3 camEulerAngles = playerCam.eulerAngles;
+            bool turningLeft = Input.GetKey("a");
+            bool turningRight = Input.GetKey("d");
+
             if ((turningLeft && turningRight) || (!turningLeft && !turningRight))
             {
-                playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, 0);
+                ResetCamTilt();
                 return;
             }
             
@@ -42,34 +56,65 @@ namespace BossFight.Cyber
             {
                 // Turning speed should be linear to velocity of player
                 Vector3 newPos = new Vector3(cur.x + turnSpeed * velocity * (1f / 100f) * Time.deltaTime,
-                                             cur.y,
-                                             cur.z);
+                    cur.y,
+                    cur.z);
                 this.transform.position = ClampPlayerX(newPos);
-                playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, -camTiltAngleWhileTurn);
+                TiltCamLeft();
             }
             if (turningRight)
             {
                 Vector3 newPos = new Vector3(cur.x - turnSpeed * velocity * (1f / 100f) * Time.deltaTime, 
-                                               cur.y,
-                                               cur.z);
+                    cur.y,
+                    cur.z);
                 this.transform.position = ClampPlayerX(newPos);
-                playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, camTiltAngleWhileTurn);
+                TiltCamRight();
             }
         }
-
+        
         private Vector3 ClampPlayerX(Vector3 pos)
         {
             float clampedX = Mathf.Clamp(pos.x,
-                                    streetBounds.GetMinX() + (GetMeshWidth() / 2), 
-                                    streetBounds.GetMaxX() - (GetMeshWidth() / 2));
+                streetBounds.GetMinX() + (GetMeshWidth() / 2), 
+                streetBounds.GetMaxX() - (GetMeshWidth() / 2));
             return new Vector3(clampedX, pos.y, pos.z);
+        }
+
+        private void ResetCamTilt()
+        {
+            Vector3 camEulerAngles = playerCam.eulerAngles;
+            playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, 0);
+        }
+
+        private void TiltCamLeft()
+        {
+            Vector3 camEulerAngles = playerCam.eulerAngles;
+            playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, -camTiltAngleWhileTurn);
+        }
+        
+        private void TiltCamRight()
+        {
+            Vector3 camEulerAngles = playerCam.eulerAngles;
+            playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, camTiltAngleWhileTurn);
         }
 
         private void OnTriggerEnter(Collider other) => BumpedIntoObject();
 
         private void BumpedIntoObject()
         {
-            Debug.Log("Bumped into Object");
+            if (playerStunned)
+            {
+                return;
+            }
+            playerStunned = true;
+            ResetCamTilt();
+            velocity = velocityAfterCrash;
+            StartCoroutine(EndPlayerStun());
+        }
+
+        private IEnumerator EndPlayerStun()
+        {
+            yield return new WaitForSeconds(this.crashStunTime);
+            playerStunned = false;
         }
     }
 }
