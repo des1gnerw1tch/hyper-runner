72a940e6a975a7e7e0b17ecb734408bcf94920ac
des1gnerw1tch <zachcanon@gmail.com>
2024-08-08 22:25:28 -0400

    Abstracted racing car controller out of model

---
+++ Hyper Runner/Assets/Scripts/BossFight/Cyber/ARacingCar.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using System.Collections;
+
+namespace BossFight.Cyber
+{
+    public abstract class ARacingCar : AObjectWithMeshWidth
+    {
+        [SerializeField] private float turnSpeed;
+        
+        [SerializeField] private StreetBounds streetBounds;
+        [SerializeField] private float crashStunTime;
+        [SerializeField] private float velocityAfterCrash;
+
+        [SerializeField] private GameObject mesh;
+        [SerializeField] private float timeToSpinAfterCrashAnim;
+        
+        [SerializeField]
+        protected float velocity = 0;
+
+        private bool playerStunned = false;
+
+        protected enum TurningDirection
+        {
+            Left,
+            Right,
+            None
+        }
+        
+        private void Update()
+        {
+            if (!playerStunned)
+            {
+                HandlePlayerSpeedInput();
+            }
+            velocity = Mathf.Clamp(velocity, 0, Mathf.Infinity);
+            this.transform.Translate(Vector3.back * velocity *  Time.deltaTime);
+            if (!playerStunned)
+            {
+                HandlePlayerTurning();
+            }
+        }
+
+        protected abstract void HandlePlayerSpeedInput();
+
+        private void HandlePlayerTurning()
+        {
+            TurningDirection direction = GetTurningDirectionInput();
+            Vector3 cur = transform.position;
+            if (direction == TurningDirection.Left)
+            {
+                // Turning speed should be linear to velocity of player
+                Vector3 newPos = new Vector3(cur.x + turnSpeed * velocity * (1f / 100f) * Time.deltaTime,
+                    cur.y,
+                    cur.z);
+                this.transform.position = ClampPlayerX(newPos);
+            }
+            else if (direction == TurningDirection.Right)
+            {
+                Vector3 newPos = new Vector3(cur.x - turnSpeed * velocity * (1f / 100f) * Time.deltaTime, 
+                    cur.y,
+                    cur.z);
+                this.transform.position = ClampPlayerX(newPos);
+            }
+        }
+
+        protected abstract TurningDirection GetTurningDirectionInput();
+
+        private Vector3 ClampPlayerX(Vector3 pos)
+        {
+            float clampedX = Mathf.Clamp(pos.x,
+                streetBounds.GetMinX() + (GetMeshWidth() / 2), 
+                streetBounds.GetMaxX() - (GetMeshWidth() / 2));
+            return new Vector3(clampedX, pos.y, pos.z);
+        }
+
+        private void OnTriggerEnter(Collider other) => BumpedIntoObject();
+
+        protected virtual void BumpedIntoObject()
+        {
+            if (playerStunned)
+            {
+                return;
+            }
+            playerStunned = true;
+            velocity = velocityAfterCrash;
+            StartCoroutine(EndPlayerStun());
+            StartCoroutine(StartSpinAnim());
+        }
+
+        private IEnumerator EndPlayerStun()
+        {
+            yield return new WaitForSeconds(this.crashStunTime);
+            playerStunned = false;
+        }
+
+        private IEnumerator StartSpinAnim()
+        {
+            float timeElapsed = 0f;
+            float progress = timeElapsed / timeToSpinAfterCrashAnim;
+            while (progress <= 1f)
+            {
+                this.mesh.transform.localRotation = Quaternion.AngleAxis(Mathf.Lerp(0, 360, timeElapsed / timeToSpinAfterCrashAnim), Vector3.up);
+                timeElapsed += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            mesh.transform.localRotation = Quaternion.AngleAxis(0, Vector3.up);
+        }
+    }
+}
--- Hyper Runner/Assets/Scripts/BossFight/Cyber/PlayerCar.cs
+++ Hyper Runner/Assets/Scripts/BossFight/Cyber/PlayerCar.cs
@@ -1,84 +1,49 @@
-using System.Collections;
 using UnityEngine;
 
 namespace BossFight.Cyber
 {
-    public class PlayerCar : AObjectWithMeshWidth
+    public class PlayerCar : ARacingCar
     {
         [SerializeField] private float acceleration;
         [SerializeField] private float brakeAcceleration;
-        [SerializeField] private float turnSpeed;
         
         [SerializeField] private Transform playerCam;
         [SerializeField] private float camTiltAngleWhileTurn;
-        [SerializeField] private StreetBounds streetBounds;
-        [SerializeField] private float crashStunTime;
-        [SerializeField] private float velocityAfterCrash;
 
-        [SerializeField] private GameObject mesh;
-        [SerializeField] private float timeToSpinAfterCrashAnim;
-        
-        [SerializeField]
-        private float velocity = 0;
-
-        private bool playerStunned = false;
-        
-        private void Update()
-        {
-            if (Input.GetKey("w") && !playerStunned)
-            {
-                velocity += acceleration;
-            }
-            if (Input.GetKey("s") && !playerStunned)
-            {
-                velocity -= brakeAcceleration;
-            }
-            
-            this.transform.Translate(Vector3.back * velocity *  Time.deltaTime);
-            if (playerStunned)
-            {
-                return;
-            }
-            HandlePlayerTurning();
-        }
-
-        private void HandlePlayerTurning()
+        protected override TurningDirection GetTurningDirectionInput()
         {
             bool turningLeft = Input.GetKey("a");
             bool turningRight = Input.GetKey("d");
-
             if ((turningLeft && turningRight) || (!turningLeft && !turningRight))
             {
                 ResetCamTilt();
-                return;
+                return TurningDirection.None;
             }
-            
-            Vector3 cur = transform.position;
             if (turningLeft)
             {
-                // Turning speed should be linear to velocity of player
-                Vector3 newPos = new Vector3(cur.x + turnSpeed * velocity * (1f / 100f) * Time.deltaTime,
-                    cur.y,
-                    cur.z);
-                this.transform.position = ClampPlayerX(newPos);
                 TiltCamLeft();
+                return TurningDirection.Left;
             }
-            if (turningRight)
-            {
-                Vector3 newPos = new Vector3(cur.x - turnSpeed * velocity * (1f / 100f) * Time.deltaTime, 
-                    cur.y,
-                    cur.z);
-                this.transform.position = ClampPlayerX(newPos);
-                TiltCamRight();
-            }
+            TiltCamRight();
+            return TurningDirection.Right;
         }
-        
-        private Vector3 ClampPlayerX(Vector3 pos)
+
+        protected override void BumpedIntoObject()
         {
-            float clampedX = Mathf.Clamp(pos.x,
-                streetBounds.GetMinX() + (GetMeshWidth() / 2), 
-                streetBounds.GetMaxX() - (GetMeshWidth() / 2));
-            return new Vector3(clampedX, pos.y, pos.z);
+            base.BumpedIntoObject();
+            ResetCamTilt();
+        }
+
+        protected override void HandlePlayerSpeedInput()
+        {
+            if (Input.GetKey("w"))
+            {
+                velocity += acceleration;
+            }
+            if (Input.GetKey("s"))
+            {
+                velocity -= brakeAcceleration;
+            }
         }
 
         private void ResetCamTilt()
@@ -97,40 +62,6 @@ namespace BossFight.Cyber
         {
             Vector3 camEulerAngles = playerCam.eulerAngles;
             playerCam.rotation = Quaternion.Euler(camEulerAngles.x, camEulerAngles.y, camTiltAngleWhileTurn);
-        }
-
-        private void OnTriggerEnter(Collider other) => BumpedIntoObject();
-
-        private void BumpedIntoObject()
-        {
-            if (playerStunned)
-            {
-                return;
-            }
-            playerStunned = true;
-            ResetCamTilt();
-            velocity = velocityAfterCrash;
-            StartCoroutine(EndPlayerStun());
-            StartCoroutine(StartSpinAnim());
-        }
-
-        private IEnumerator EndPlayerStun()
-        {
-            yield return new WaitForSeconds(this.crashStunTime);
-            playerStunned = false;
-        }
-
-        private IEnumerator StartSpinAnim()
-        {
-            float timeElapsed = 0f;
-            float progress = timeElapsed / timeToSpinAfterCrashAnim;
-            while (progress <= 1f)
-            {
-                this.mesh.transform.localRotation = Quaternion.AngleAxis(Mathf.Lerp(0, 360, timeElapsed / timeToSpinAfterCrashAnim), Vector3.up);
-                timeElapsed += Time.deltaTime;
-                yield return new WaitForEndOfFrame();
-            }
-            mesh.transform.localRotation = Quaternion.AngleAxis(0, Vector3.up);
         }
     }
 }
