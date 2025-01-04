82006054d7455765f995774d5cc910528c44d7d1
des1gnerw1tch <zachcanon@gmail.com>
2025-01-03 19:21:42 -0500

    Ocean tiles will tile correctly without seams

--- Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
+++ Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
@@ -1,9 +1,15 @@
 #ifndef MYHLSLINCLUDE_INCLUDED
 #define MYHLSLINCLUDE_INCLUDED
 
-void MakeWaves_float(float3 inPos, float simpleNoise, out float3 outPos)
+void MakeWaves_float(float3 inPos, float simpleNoise, float2 uv, out float3 outPos)
 {
-    const float waveMaxHeight = 10.0f;
+    float waveMaxHeight = 8.0f;
+    // If near the edge, make wave height 0. This is so that when we repeat/tile ocean plane, the waves will match up.
+    if (uv.y > 0.9 || uv.y < 0.1)
+    {
+        waveMaxHeight = 0.0f;
+    }
+    
     outPos = float3(inPos.x, inPos.y + (simpleNoise * waveMaxHeight) - (waveMaxHeight/2), inPos.z);
 }
 
