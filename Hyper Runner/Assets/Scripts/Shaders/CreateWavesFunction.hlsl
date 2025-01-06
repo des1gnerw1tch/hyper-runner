dcd9588a828de2f3561ca53897072337a7151026
des1gnerw1tch <zachcanon@gmail.com>
2025-01-05 22:54:38 -0500

    Ocean has more verts for waves

--- Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
+++ Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
@@ -3,7 +3,7 @@
 
 void MakeWaves_float(float3 inPos, float simpleNoise, float2 uv, out float3 outPos)
 {
-    float waveMaxHeight = 8.0f;
+    float waveMaxHeight = 4.0f;
     // If near the edge, make wave height 0. This is so that when we repeat/tile ocean plane, the waves will match up.
     if (uv.y > 0.9 || uv.y < 0.1)
     {
