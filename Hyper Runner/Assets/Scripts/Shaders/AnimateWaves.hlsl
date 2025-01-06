a75c8e10accee87a05a572bfb8d6b0b74b078f68
des1gnerw1tch <zachcanon@gmail.com>
2025-01-06 02:03:36 -0500

    Started calculation of surface normals for waves

--- Hyper Runner/Assets/Scripts/Shaders/AnimateWaves.hlsl
+++ Hyper Runner/Assets/Scripts/Shaders/AnimateWaves.hlsl
@@ -1,12 +1,23 @@
 #ifndef ANIMATEWAVES_INCLUDED
 #define ANIMATEWAVES_INCLUDED
 
-// Takes in UV coordinate and exports animated UV dependent on time
-void AnimateWaves_float(float2 uv, out float2 animatedUV)
+// Takes in UV coordinate and exports animated UV dependent on time. Also outputs UV to right and upwards
+// so can do normal calculation
+void AnimateWaves_float(float2 uv, float uvDistanceForNormalCalc, out float2 animatedUV, out float2 uvToRight, out float2 uvUpwards)
 {
-    float2 newUV = float2(uv.x + _Time.x, uv.y); // Water flows to the right
-    newUV.x -= floor(newUV.x);
-    animatedUV = newUV;
+    float2 animatedUV_ = float2(uv.x + _Time.x, uv.y); // Water flows to the right
+    float2 uvToRight_ = float2(animatedUV_.x + uvDistanceForNormalCalc, animatedUV_.y);
+    float2 uvUpwards_ = float2(animatedUV_.x, animatedUV_.y + uvDistanceForNormalCalc);
+    
+    animatedUV_.x -= floor(animatedUV_.x);
+    animatedUV = animatedUV_;
+    
+    uvToRight_.x -= floor(uvToRight_.x);
+    uvToRight = uvToRight_;
+    
+    uvUpwards_.y -= floor(uvUpwards_.y);
+    uvUpwards = uvUpwards_;
+    
 }
 
 #endif
--- Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
+++ Hyper Runner/Assets/Scripts/Shaders/CreateWavesFunction.hlsl
@@ -1,7 +1,8 @@
 #ifndef CREATEWAVES_INCLUDED
 #define CREATEWAVES_INCLUDED
 
-void MakeWaves_float(float3 inPos, float simpleNoise, float2 uv, out float3 outPos)
+void MakeWaves_float(float3 inPos, float simpleNoise, float noiseToRight,
+    float noiseUpwards, float2 uv, out float3 outPos, out float3 normal)
 {
     float waveMaxHeight = 4.0f;
     // If near the edge, make wave height 0. This is so that when we repeat/tile ocean plane, the waves will match up.
@@ -11,6 +12,13 @@ void MakeWaves_float(float3 inPos, float simpleNoise, float2 uv, out float3 outP
     }
     
     outPos = float3(inPos.x, inPos.y + (simpleNoise * waveMaxHeight) - (waveMaxHeight/2), inPos.z);
+
+    // calculate normal for lighting
+    float3 rightPos = float3(inPos.x + 1, inPos.y + (noiseToRight * waveMaxHeight) - (waveMaxHeight/2), inPos.z);
+    float3 upPos = float3(inPos.x, inPos.y + (noiseUpwards * waveMaxHeight) - (waveMaxHeight/2), inPos.z - 1);
+    float3 dir1 = normalize(rightPos - outPos);
+    float3 dir2 = normalize(upPos - outPos);
+    normal = normalize(cross(dir1, dir2));
 }
 
 #endif
