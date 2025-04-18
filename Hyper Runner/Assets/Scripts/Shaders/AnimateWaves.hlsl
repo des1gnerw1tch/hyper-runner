1283a1f91bf89cd22547ade37b425fba5475d93d
des1gnerw1tch <zachcanon@gmail.com>
2025-04-18 00:50:25 -0400

    Small touchups while writing blog

--- Hyper Runner/Assets/Scripts/Shaders/AnimateWaves.hlsl
+++ Hyper Runner/Assets/Scripts/Shaders/AnimateWaves.hlsl
@@ -5,19 +5,14 @@
 // so can do normal calculation
 void AnimateWaves_float(float2 uv, float uvDistanceForNormalCalc, out float2 animatedUV, out float2 uvToRight, out float2 uvUpwards)
 {
-    float2 animatedUV_ = float2(uv.x + _Time.x * 3, uv.y); // Water flows to the right
+    float speed = 3.0f;
+    float2 animatedUV_ = float2(uv.x + _Time.x * speed, uv.y); // Water flows to the right
     float2 uvToRight_ = float2(animatedUV_.x + uvDistanceForNormalCalc, animatedUV_.y);
     float2 uvUpwards_ = float2(animatedUV_.x, animatedUV_.y + uvDistanceForNormalCalc);
     
-    //animatedUV_.x -= floor(animatedUV_.x);
     animatedUV = animatedUV_;
-    
-   // uvToRight_.x -= floor(uvToRight_.x);
     uvToRight = uvToRight_;
-    
-   //  uvUpwards_.y -= floor(uvUpwards_.y);
     uvUpwards = uvUpwards_;
-    
 }
 
 #endif
